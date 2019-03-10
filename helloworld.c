836e151c3451f6d9ff372696c0c6c3eede87b3de
nithin <nithss@gmail.com>
2019-03-10 22:33:21 +0530

    Added function for adding floating point numbers

--- helloworld.c
+++ helloworld.c
@@ -7,13 +7,18 @@ int sum( int a, int b )
     return a+b;
 }
 
+float fsum(float a, float b)
+{
+	return a+b;
+}
 
 
 /*The main function starts here*/
 void main()
 {
-
    printf("This is my first git project\n");
+   sum(1, 2);
+   fsum(1.2, 3.4);
 }
 
 
